3e94f9c8598fc50a7e19899f4536a645f37cf721
alfabar <alfa_bar@hotmail.com>
2021-12-08 09:58:31 -0300

    fontawesone

--- includes/header.php
+++ includes/header.php
@@ -16,10 +16,34 @@
   <meta name="description" content="Encontre seu emprego aqui de forma totalmente gratuita">
   <!-- Palavras chaves Quanto menos palavras chaves melhor)-->
   <meta name="keywords" content="Emprego, Trabalho">
-
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!-- Navbar -->
--- includes/rodape.php
+++ includes/rodape.php
@@ -4,27 +4,27 @@
         <!-- Section: Social media -->
         <section class="mb-4">
             <!-- Facebook -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href="#!" role="button"><i
-                    class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #3b5998;" href="#!" role="button"><i
+                    class="fa fa-facebook-f"></i></a>
 
             <!-- Twitter -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i
-                    class="fab fa-twitter"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #55acee;" href="#!" role="button"><i
+                    class="fa fa-twitter"></i></a>
 
             <!-- Google -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="#!" role="button"><i
-                    class="fab fa-google"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #dd4b39;" href="#!" role="button"><i
+                    class="fa fa-google"></i></a>
 
             <!-- Instagram -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i
-                    class="fab fa-instagram"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #ac2bac;" href="#!" role="button"><i
+                    class="fa fa-instagram"></i></a>
 
             <!-- Linkedin -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i
-                    class="fab fa-linkedin-in"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #0082ca;" href="#!" role="button"><i
+                    class="fa fa-linkedin-in"></i></a>
             <!-- Github -->
-            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i
-                    class="fab fa-github"></i></a>
+            <a class="btn btn-primary m-1" style="background-color: #333333;" href="#!" role="button"><i
+                    class="fa fa-github"></i></a>
         </section>
         <!-- Section: Social media -->
     </div>
