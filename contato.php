b577dbeef8b1cc29ccf2d0f28c3d9219037b4b36
alfabar <alfa_bar@hotmail.com>
2021-12-08 11:21:33 -0300

    atualizado

--- contato.php
+++ contato.php
@@ -39,7 +39,7 @@
     --><div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Entre em contato</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <p class="lead">Informe o motivo de seu contato</p>
   </div>
 </div>
     <form>
--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
 
 <body>
 <header class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="index.php">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -31,16 +31,16 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="vagas.php">Vagas</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="contato.php" title="fale com a equipe do start jobs">Contatos</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+      <a class="nav-link" href="sobre.php">Sobre</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
