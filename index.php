ff638575277edc70058d55be668e4c497812d57a
alfabar <alfa_bar@hotmail.com>
2021-12-08 10:16:44 -0300

    atualizar

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
   </header> -->
   <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+      <h1 class="display-4 font-italic">Star Jobs Sua carreira começa aqui</h1>
+      <p class="lead my-3">Multiplas empresas publicam diariamente ofertas de emprego e freelancers trabalhos esporadicos .</p>
+      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue Lendo...</a></p>
     </div>
   </div>
 
