4c7a89ae69cbb95e0ec5d097efb7da312b56ba85
Tiago Ferreira <tiagoferreira18@gmail.com>
2021-12-08 12:02:46 -0300

    logo

--- includes/header.php
+++ includes/header.php
@@ -18,37 +18,41 @@
   <meta name="keywords" content="Emprego, Trabalho">
   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/styles.css">
 
 </head>
 
 <body>
-<header class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <header class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- <a class="navbar-brand" href="index.php">Navbar</a> -->
+    <h1 class="logo">
+      <a href="index.php">Start<br><span>Jobs</span></a>
+    </h1>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="vagas.php">Vagas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contato.php" title="fale com a equipe do start jobs">Contatos</a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link" href="sobre.php">Sobre</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Procurar">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Procurar vagas</button>
-    </form>
-  </div>
-</header>
-<div class="container">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vagas.php">Vagas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php" title="fale com a equipe do start jobs">Contatos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sobre.php">Sobre</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Procurar">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Procurar vagas</button>
+      </form>
+    </div>
+  </header>
+  <div class="container">
 
-<!-- Navbar -->
+    <!-- Navbar -->
